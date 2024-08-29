676895dc085d745138124d0d74c0c7f3da064e2f
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-29 15:03:41 +0300

    Set service2 project as the gRPC server that gets and handles the request!
    Successful HelloReply is sent back to the gRPC client in
    "quarkus-grpc-task" project

--- src/main/java/com/sumerge/ExampleResource.java
+++ src/main/java/com/sumerge/ExampleResource.java
@@ -5,12 +5,13 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/goodbye")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Goodbye from Rest";
     }
+
 }
--- src/test/java/com/sumerge/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.sumerge;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
-}
